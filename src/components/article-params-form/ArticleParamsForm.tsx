ec554a381e500037eb288af4ff5a262ba771d5db
d.khurani <huranidara@gmail.com>
2024-04-16 19:16:28 +0300

    test

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,8 +4,15 @@ import { Button } from 'components/button';
 import styles from './ArticleParamsForm.module.scss';
 
 import React, { useState } from 'react';
+import { Select } from 'components/select';
+import * as articleProps from 'src/constants/articleProps';
+import { Simulate } from 'react-dom/test-utils';
+import select = Simulate.select;
+import { Text } from 'components/text';
+import { defaultArticleState, OptionType } from 'src/constants/articleProps';
 
 export const ArticleParamsForm = () => {
+	const [formState, setFormState] = useState(defaultArticleState);
 	const [isOpen, setIsOpen] = useState(false);
 
 	const togglePanel = () => {
@@ -20,6 +27,17 @@ export const ArticleParamsForm = () => {
 					isOpen ? styles.container_open : ''
 				}`}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase family='open-sans'>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						onChange={(value: OptionType) =>
+							setFormState({ ...formState, fontFamilyOption: value })
+						}
+						options={articleProps.fontFamilyOptions}
+						title='Font Family'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
